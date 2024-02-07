294ae4da3c30e333be0a5525abc1b47326c7148c
fauzanalghifary <m.fauzanghifary@gmail.com>
2024-02-07 15:41:31 +0700

    finish section 06

--- resources/store.py
+++ resources/store.py
@@ -1,9 +1,11 @@
 import uuid
 from flask import request
 from flask_smorest import abort, Blueprint
-from db import stores
 from flask.views import MethodView
+from sqlalchemy.exc import SQLAlchemyError, IntegrityError
 
+from db import db
+from models import StoreModel
 from schemas import StoreSchema
 
 blp = Blueprint("stores", __name__, description="Operations on stores")
@@ -13,36 +15,32 @@ blp = Blueprint("stores", __name__, description="Operations on stores")
 class Store(MethodView):
     @blp.response(200, StoreSchema)
     def get(self, store_id):
-        try:
-            return stores[store_id]
-        except KeyError:
-            abort(404, message='Store not found')
+        store = StoreModel.query.get_or_404(store_id)
+        return store
 
     def delete(self, store_id):
-        try:
-            del stores[store_id]
-            return {'message': 'Store deleted'}
-        except KeyError:
-            abort(404, message='Store not found')
+        store = StoreModel.query.get_or_404(store_id)
+        db.session.delete(store)
+        db.session.commit()
+        return {"message": "Store deleted"}
 
 
 @blp.route('/store')
 class Stores(MethodView):
     @blp.response(200, StoreSchema(many=True))
     def get(self):
-        return stores.values()
+        return StoreModel.query.all()
 
     @blp.arguments(StoreSchema)
     @blp.response(200, StoreSchema)
     def post(self, store_data):
-        for store in stores.values():
-            if store_data["name"] == store["name"]:
-                abort(400, message=f"Store already exists.")
+        store = StoreModel(**store_data)
+        try:
+            db.session.add(store)
+            db.session.commit()
+        except IntegrityError:
+            abort(400, message="A store with that name already exist.")
+        except SQLAlchemyError:
+            abort(500, message="An error occurred while inserting the store.")
 
-        store_id = uuid.uuid4().hex
-        store = {
-            **store_data,
-            'id': store_id,
-        }
-        stores[store_id] = store
         return store
